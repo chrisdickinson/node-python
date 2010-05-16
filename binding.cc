1dd7bcb1d8e8a1d234318f4f65d2342936fa32a7
Chris Dickinson <christopher.s.dickinson@gmail.com>
2010-05-15 19:14:42 -0500

    some progress. two steps forward, one step back?

--- binding.cc
+++ binding.cc
@@ -1,6 +1,79 @@
 #include <v8.h>
 #include <python2.6/Python.h>
+#include <iostream>
+#include <string>
 using namespace v8;
+using std::string;
+
+class PythonObject {
+    protected:
+        PyObject* _object;
+    public:
+        PythonObject(PyObject* pyobj): _object(pyobj) { 
+
+        }
+        virtual ~PythonObject() { Py_XDECREF(_object); } 
+};
+ 
+class JSPythonObject : public PythonObject {
+    public:
+        JSPythonObject(PyObject* obj) : PythonObject(obj) { }
+
+        static Handle<Value> MapGet(Local<String> key, const AccessorInfo& info) {
+            HandleScope handle_scope;
+            JSPythonObject* object = UnwrapPythonObject(info.Holder());
+            String::Utf8Value utf8_key(key->ToString());
+            std::cout << *utf8_key << '\t';
+            if(PyObject_HasAttrString(object->_object, *utf8_key)) {
+                PyObject* python_attribute = PyObject_GetAttrString(object->_object, *utf8_key);
+                if(python_attribute != NULL) {
+                    std::cout << "FOUND\n";
+                    JSPythonObject* jspython = new JSPythonObject(python_attribute);
+                    return handle_scope.Close(WrapPythonObject(jspython));
+                }
+            }
+/*
+            if(std::string("toString") == std::string(*utf8_key)) {
+                Local<FunctionTemplate> toString = FunctionTemplate::New(ToString);
+                return handle_scope.Close(toString);
+            }*/
+            std::cout << "NOT FOUND\n";
+
+            return Undefined();
+        }
+/*
+        static Handle<String> ToString(const Arguments& args) {
+            HandleScope handle_scope;
+            Handle<Object> wrapped = args.This();
+            JSPythonObject* obj = JSPythonObject::UnwrapPythonObject(wrapped);
+            if(PyString_Check(obj->_object)) {
+                char* cstr = PyString_AsString(obj->_object);
+                return handle_scope.Close(String::New(PyString_AsString(obj->_object)));
+            }
+            return handle_scope.Close(String::New("Python object"));
+        }
+*/
+        static Handle<Value> MapSet(Local<String> key, Local<Value> value, const AccessorInfo& info) {
+            return Undefined();
+        }
+
+        static Handle<Object>   WrapPythonObject(JSPythonObject* obj) {
+            HandleScope handle_scope;
+            Handle<ObjectTemplate> tpl = python_object_template_;
+            Handle<Object> result = tpl->NewInstance();
+            Handle<External> py_ptr = External::New(obj);
+            result->SetInternalField(0, py_ptr);
+            return handle_scope.Close(result);
+        }
+        static JSPythonObject*  UnwrapPythonObject(Handle<Object> obj) {
+            Handle<External> field = Handle<External>::Cast(obj->GetInternalField(0));
+            void* ptr = field->Value();
+            return static_cast<JSPythonObject*>(ptr);
+        }
+        static Persistent<ObjectTemplate> python_object_template_;
+};
+
+Persistent<ObjectTemplate> JSPythonObject::python_object_template_;
 
 static Handle<Value>
 Import (const Arguments& args) {
@@ -15,27 +88,7 @@ Import (const Arguments& args) {
     *py_module = PyImport_Import(py_module_name);
     Py_DECREF(py_module_name);
     if(py_module != NULL) {
-
-
-        PyObject* py_path = PyObject_Dir(py_module);
-        int sys_path_length = PyList_Size(py_path), 
-        i;
-        char* cstr;
-
-        Local<Array> output = Array::New(sys_path_length);
-        for(i = 0; i < sys_path_length; ++i) {
-            PyObject* item = PyList_GetItem(py_path, i);
-            if(!PyArg_Parse(item, "s", &cstr)) {
-                
-            } else {
-                output->Set(Number::New(i), String::New(cstr));
-            }
-            Py_DECREF(item);
-        }
-
-        Py_XDECREF(py_path);
-        Py_DECREF(py_module);
-        return scope.Close(output);
+        return scope.Close(JSPythonObject::WrapPythonObject(new JSPythonObject(py_module))); 
     }
     return ThrowException(
         Exception::TypeError(String::New("Could not import that module."))
@@ -53,6 +106,12 @@ init (Handle<Object> target) {
     HandleScope scope;
     Py_Initialize();
     // create a new function template and assign it to the exported variable "fibonacci"
+    Handle<ObjectTemplate> obj_template = ObjectTemplate::New();
+    obj_template->SetInternalFieldCount(1);
+    obj_template->SetNamedPropertyHandler(JSPythonObject::MapGet, JSPythonObject::MapSet);
+
+    JSPythonObject::python_object_template_ = Persistent<ObjectTemplate>::New(obj_template);
+
     Local<FunctionTemplate> import = FunctionTemplate::New(Import);
     Local<FunctionTemplate> shutdown = FunctionTemplate::New(Shutdown);
     target->Set(String::New("import"), import->GetFunction());
--- test.js
+++ test.js
@@ -2,12 +2,8 @@ var sys = require('sys'),
     puts = sys.puts,
     binding = require('./binding');
 
-var items;
-var modules = ['sys', 'os', 'StringIO'];
-for (var index in modules) {
-    items = binding.import(modules[index]);
-    puts(modules[index] + ":\t" + items.join(', '));
-    puts('----');
-}
+var py_sys = binding.import('sys');
+var something = py_sys.version
 
+puts(something);
 binding.shutdown();
